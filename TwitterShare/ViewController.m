cd8b878048a4fb5f66f313d7e5454f143b8612f6
Sergey Lavrov <lavrov-sergey@yandex.ru>
2019-02-25 02:03:19 +0300

    Кнопки для Twitter и Facebook заработали
    Оставшиеся 2 кнопки пока ничего не делают, т.к. пока почему-то возникает ошибка.

--- TwitterShare/ViewController.m
+++ TwitterShare/ViewController.m
@@ -12,8 +12,8 @@
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextView *tweetTextView;
-@property (weak, nonatomic) IBOutlet UITextView *facebookTextiView;
-@property (weak, nonatomic) IBOutlet UITextView *popupTextView;
+@property (weak, nonatomic) IBOutlet UITextView *facebookTextView;
+@property (weak, nonatomic) IBOutlet UITextView *moreTextView;
 
 - (void) configureTextView :(int) typeView :(UITextView *) view;
 - (void) showAlertMessage :(NSString *) myMessage;
@@ -24,104 +24,106 @@
 
 int const TWEET_VIEW = 1;
 int const FACEBOOK_VIEW = 2;
-int const POPUP_VIEW = 3;
+int const MORE_VIEW = 3;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Оформление TextView
     [self configureTextView :TWEET_VIEW :self.tweetTextView];
-    [self configureTextView :FACEBOOK_VIEW :self.facebookTextiView];
-    [self configureTextView :POPUP_VIEW :self.popupTextView];
+    [self configureTextView :FACEBOOK_VIEW :self.facebookTextView];
+    [self configureTextView :MORE_VIEW :self.moreTextView];
 }
 
 // Показ сообщения в модальном окне
 - (void) showAlertMessage: (NSString *) myMessage{
     UIAlertController *alertController;
-    alertController = [UIAlertController alertControllerWithTitle:@"Поделиться" message:myMessage preferredStyle:UIAlertControllerStyleAlert];
-    [alertController addAction:[UIAlertAction actionWithTitle:@"Ок" style:UIAlertActionStyleDefault handler:nil]];
+    alertController = [UIAlertController alertControllerWithTitle:@"Share" message:myMessage preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil]];
     [self presentViewController:alertController animated:YES completion:nil];
 }
 
+- (IBAction)tweetShareAction :(id)sender {
+    [self showShareAction :sender :TWEET_VIEW :self.tweetTextView];
+}
+- (IBAction)facebookShareAction :(id)sender {
+    [self showShareAction :sender :FACEBOOK_VIEW :self.facebookTextView];
+}
+- (IBAction)avcShareAction :(id)sender {
+    [self showShareAction :sender :MORE_VIEW :self.moreTextView];
+}
+- (IBAction)popupShareAction :(id)sender {
+    [self showShareAction :sender :MORE_VIEW :self.moreTextView];
+}
+
+
 // При нажатии на кнопку
-- (IBAction)showShareAction:(id)sender {
+- (IBAction)showShareAction :(id)sender :(int) typeView :(UITextView *) view{
     // Скрываем клавиатуру если она открыта
-    if([self.tweetTextView isFirstResponder]){
-        [self.tweetTextView resignFirstResponder];
+    if([view isFirstResponder]){
+        [view resignFirstResponder];
     }
     
-    // Настраиваем окно с сообщением
-    UIAlertController *actionController = [UIAlertController alertControllerWithTitle:@"Поделиться" message:@"Где поделиться текстом?" preferredStyle:UIAlertControllerStyleAlert];
-    
-    // Настраиваем кнопку отмены
-    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Отмена" style:UIAlertActionStyleDefault  handler:nil];
-    
-    // Настраиваем кнопку tweet
-    UIAlertAction *tweetAction = [UIAlertAction actionWithTitle:@"Твитнуть" style:UIAlertActionStyleDefault  handler:
-          ^(UIAlertAction *action){
-              if([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]){
-                  SLComposeViewController *twitterVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
-                  
-                  // Отправляем текст в Twitter (обрезая до 140 символов, если он длиннее)
-                  if([self.tweetTextView.text length] < 140){
-                      [twitterVC setInitialText:self.tweetTextView.text];
-                  } else {
-                      NSString *shortText = [self.tweetTextView.text substringToIndex:140];
-                      [twitterVC setInitialText:shortText];
-                  }
-                  [self presentViewController:twitterVC animated:YES completion:nil];
-                  
-              // Если не удалось подключиться к Twitter - сообщаем об этом
-              } else {
-                  [self showAlertMessage:@"Ты не вошёл в Twitter"];
-              }
-          }];
-    
-    // Настраиваем кнопку facebook
-    UIAlertAction *facebookAction = [UIAlertAction actionWithTitle:@"Пост в Фейсбуке" style:UIAlertActionStyleDefault  handler:^(UIAlertAction *action){
-        if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]){
-            //[self showShareAction:@"Фейсбук доступен"];
-            SLComposeViewController *facebookVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
-            [facebookVC setInitialText:self.tweetTextView.text];
-            [self presentViewController:facebookVC animated:YES completion:nil];
-        } else {
-            [self showAlertMessage:@"Войдите в Фейсбук"];
-        }
-    }];
-    
-    // Настраиваем кнопку Ещё
-    UIAlertAction *moreAction = [UIAlertAction actionWithTitle:@"Ещё" style:UIAlertActionStyleDefault  handler:^(UIAlertAction *action){
-        UIActivityViewController *moreVC = [[UIActivityViewController alloc] initWithActivityItems:@[self.tweetTextView.text] applicationActivities:nil];
-        [self presentViewController:moreVC animated:YES completion:nil];
-        
-    }];
-
-    // Добавляем кнопки
-    [actionController addAction:tweetAction];
-    [actionController addAction:facebookAction];
-    [actionController addAction:moreAction];
-    [actionController addAction:cancelAction];
-
-    // Показываем окно с сообщением
-    [self presentViewController:actionController animated:YES completion:nil];
+    switch (typeView) {
+        case TWEET_VIEW:
+            if([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]){
+                SLComposeViewController *twitterVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+                
+                // Отправляем текст в Twitter (обрезая до 140 символов, если он длиннее)
+                if([view.text length] < 140){
+                    [twitterVC setInitialText:view.text];
+                } else {
+                    NSString *shortText = [view.text substringToIndex:140];
+                    [twitterVC setInitialText:shortText];
+                }
+                [self presentViewController:twitterVC animated:YES completion:nil];
+                
+                // Если не удалось подключиться к Twitter - сообщаем об этом
+            } else {
+                [self showAlertMessage:@"Please, sign to Twitter"];
+            }
+            break;
+            
+        case FACEBOOK_VIEW:
+            if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]){
+                //[self showShareAction:@"Фейсбук доступен"];
+                SLComposeViewController *facebookVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+                [facebookVC setInitialText:view.text];
+                [self presentViewController:facebookVC animated:YES completion:nil];
+            } else {
+                [self showAlertMessage:@"Please, enter to the Facebook"];
+            }
+            break;
+            
+        case MORE_VIEW:
+            /*
+             UIActivityViewController *moreVC = [UIActivityViewController alloc];
+             [moreVC initWithActivityItems:@[view.text] applicationActivities:nil];
+             [self presentViewController:moreVC animated:YES completion:nil];
+             */
+            break;
+            
+        default:
+            break;
+    }
 }
 
 // Оформление текстового поля
 - (void) configureTextView :(int) typeView :(UITextView*) view{
     switch (typeView) {
         case TWEET_VIEW:
-            view.layer.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:0.9 alpha:1.0].CGColor;
+            view.layer.backgroundColor = [UIColor colorWithRed:0.0 green:0.7 blue:0.95 alpha:1.0].CGColor;
             view.layer.cornerRadius = 10.0;
             view.layer.borderColor = [UIColor colorWithWhite:0 alpha:0.5].CGColor;
             view.layer.borderWidth = 2.0;
             break;
         case FACEBOOK_VIEW:
-            view.layer.backgroundColor = [UIColor colorWithRed:1.0 green:0.9 blue:1.0 alpha:1.0].CGColor;
+            view.layer.backgroundColor = [UIColor colorWithRed:0.2 green:0.3 blue:0.5 alpha:1.0].CGColor;
             view.layer.cornerRadius = 20.0;
             view.layer.borderColor = [UIColor colorWithWhite:0 alpha:0.5].CGColor;
             view.layer.borderWidth = 3.0;
             break;
-        case POPUP_VIEW:
-            view.layer.backgroundColor = [UIColor colorWithRed:0.9 green:1.0 blue:1.0 alpha:1.0].CGColor;
+        case MORE_VIEW:
+            view.layer.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:0.5 alpha:1.0].CGColor;
             view.layer.cornerRadius = 30.0;
             view.layer.borderColor = [UIColor colorWithWhite:0 alpha:0.5].CGColor;
             view.layer.borderWidth = 4.0;
